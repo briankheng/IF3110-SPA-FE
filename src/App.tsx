6d0a1a802b1c9b14f2b4e59b0cfe5a5b9bd44873
billc27 <110593711+billc27@users.noreply.github.com>
2023-10-25 23:15:32 +0700

    feat: add page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// styles
 import './App.css'
+
+// pages
+import Login from "./pages/Login/login";
 
 function App() {
   return (
-    <>
-      <h1 className="h-96 text-5xl">WELCOME TO KBL - REACT TS VER.</h1>
-    </>
+    <BrowserRouter>
+      {/* <h1 className="h-96 text-5xl">WELCOME TO KBL - REACT TS VER.</h1> */}
+      <Routes>
+        {/* Example */}
+        <Route path="/" element={<Login />} />
+
+        {/* Pages */}
+
+        {/* Login Page */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Other Pages */}
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+function Login() {
+    return (
+        <div>Test Login</div>
+    );
+}
+
+export default Login;
