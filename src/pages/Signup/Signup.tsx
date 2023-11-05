1bad316c5e8019855daab87c3b938b2207c35b21
billc27 <110593711+billc27@users.noreply.github.com>
2023-11-05 18:10:58 +0700

    fix: error message size

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -43,9 +43,9 @@ function Signup() {
 
     return (
         <div className="bg-black h-screen xl:w-screen flex flex-col justify-center items-center">
-            <div className="bg-light-gray w-3/4 md:w-1/2 xl:w-1/3 overflow-y-auto px-10 py-10 xl:px-12 xl:py-16 rounded-lg text-white text-lg font-poppins">
+            <div className="bg-light-gray w-3/4 md:w-1/2 xl:w-1/3 overflow-y-auto px-10 py-10 xl:px-12 xl:py-16 rounded-lg text-white text-sm md:text-base xl:text-lg font-poppins">
                 <p className="text-center text-lg md:text-xl xl:text-2xl font-semibold">Create KBL Account</p>
-                <form onSubmit={handleSubmit} className="flex flex-col mt-8 text-sm md:text-base xl:text-lg" >
+                <form onSubmit={handleSubmit} className="flex flex-col mt-8" >
                     {/* Username */}
                     <label>USERNAME<span className="text-red-700">*</span></label>
                     <input 
@@ -82,7 +82,7 @@ function Signup() {
                     {/* Submit Button */}
                     <button className="bg-green-500 rounded-lg mt-6 py-2 text-md font-bold hover:bg-green-400" onClick={() => setIsSignupSuccess(true)}>Sign Up</button>
                 </form>
-                <p className="text-white text-sm md:text-base xl:text-lg mt-5">Have an account? <Link to="/login" className="text-light-blue hover:underline hover:text-blue-300">Login</Link></p>
+                <p className="text-white mt-5">Have an account? <Link to="/login" className="text-light-blue hover:underline hover:text-blue-300">Login</Link></p>
             </div>
 
             {/* Success Modal */}
