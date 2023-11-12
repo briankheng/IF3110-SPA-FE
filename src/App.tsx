eb96df9af156f87f55d14ee946d1a8271a2cd217
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-11-12 21:41:36 +0700

    feat : basic design trial of SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import router from "./routes";
+// styles
+import "./App.css";
 
-const App = () => {
-  return <RouterProvider router={router} />;
-};
+// pages
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+// import RedeemToken from "./pages/RedeemToken/RedeemToken";
+import AlbumDetail from "./pages/AlbumDetail";
+import Home from "./pages/Home";
+import Album from "./pages/Album";
+
+function App() {
+  return (
+    // Notes: Nanti dibuat folder routes terpisah..
+    <Router>
+      <h1> KBL-SPA CEK CEKKK </h1>
+      <Link to="/">Home</Link>
+      <Link to="/album">Album</Link>
+      <Link to="/detail">Detail</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/album" element={<Album />} />
+        <Route path="/detail" element={<AlbumDetail />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
