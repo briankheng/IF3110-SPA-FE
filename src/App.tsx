d48bf16850821e48dede7c18c0783536939f20af
billc27 <110593711+billc27@users.noreply.github.com>
2023-11-16 21:24:31 +0700

    feat: setup toastify

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,27 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
